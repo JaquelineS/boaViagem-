245d5a1fb02d8f02067bf9034b185ff127fea6d5
Rafael Brazão <brazaor@gmail.com>
2017-04-28 11:38:06 -0300

    incluindo mudancas apos a aula do dia 26/04

--- app/src/main/java/br/edu/usj/boaviagem/DashboardActivity.java
+++ app/src/main/java/br/edu/usj/boaviagem/DashboardActivity.java
@@ -3,6 +3,9 @@ package br.edu.usj.boaviagem;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by rafael on 12/04/17.
@@ -14,5 +17,12 @@ public class DashboardActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_dashboard);
+    }
+
+    public void selecionarOpcao(View view) {
+        TextView textView = (TextView) view;
+        String opcao = "Opção: "+textView.getText().toString();
+        Toast.makeText(this, opcao, Toast.LENGTH_LONG).
+                show();
     }
 }
