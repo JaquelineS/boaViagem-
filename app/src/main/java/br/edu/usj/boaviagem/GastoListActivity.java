4e5a3bb2016adbda53e868d6ff22f5a747f8f7f3
Rafael Brazão <brazaor@gmail.com>
2017-05-22 01:58:41 -0300

    alterações após a aula do dia 17/05

--- app/src/main/java/br/edu/usj/boaviagem/GastoListActivity.java
+++ app/src/main/java/br/edu/usj/boaviagem/GastoListActivity.java
@@ -7,13 +7,17 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by rafael on 10/05/17.
@@ -21,31 +25,66 @@ import java.util.List;
 
 public class GastoListActivity extends ListActivity {
 
+    private List<Map<String, Object>> gastos;
+
     private AdapterView.OnItemClickListener listener =
             new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView,
-                                        View view, int position, long id) {
-                    TextView textView = (TextView) view;
-                    Toast.makeText(getApplicationContext(), "Gasto selecionado:" +
-                    textView.getText(), Toast.LENGTH_SHORT).show();
+                                        View view, int position,
+                                        long id) {
+                    Map<String, Object> mapa = gastos.get(position);
+                    String descricao = (String) mapa.get("descricao");
+
+                    String mensagem = "Gasto selecionado "+descricao;
+
+                    Toast.makeText(getApplicationContext(), mensagem,
+                            Toast.LENGTH_SHORT).show();
                 }
             };
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(
+            @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1,
-                listarGastos()
-        ));
+
+        gastos = listarGastos();
+        String[] de = {"data", "descricao", "valor", "categoria"};
+        int[] para = {R.id.iddata, R.id.iddescricao,
+            R.id.idvalor, R.id.idcategoria};
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                gastos,
+                R.layout.layout_lista_gasto,
+                de,
+                para);
+
+        setListAdapter(adapter);
         ListView listView = getListView();
         listView.setOnItemClickListener(listener);
     }
 
-    private List<String> listarGastos(){
-        return Arrays.asList("Sanduiche R$ 19,90",
-                "Taxi Aeroporto - Hotel R$ 34,00",
-                "Revista R$ 12,00");
+    private List<Map<String, Object>> listarGastos(){
+
+        List<Map<String, Object>> gastos =
+                new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> item =
+                new HashMap<String, Object>();
+        item.put("data", "24/05/2017");
+        item.put("descricao", "Lanche");
+        item.put("valor", "R$ 20,00");
+        item.put("categoria", R.color.categoria_alimentacao);
+        gastos.add(item);
+
+        item = new HashMap<String, Object>();
+        item.put("data", "31/05/2017");
+        item.put("descricao", "Hotel");
+        item.put("valor", "R$ 200,00");
+        item.put("categoria", R.color.categoria_hospedagem);
+        gastos.add(item);
+
+        return gastos;
     }
 }
--- app/src/main/java/br/edu/usj/boaviagem/ViagemListActivity.java
+++ app/src/main/java/br/edu/usj/boaviagem/ViagemListActivity.java
@@ -6,19 +6,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by rafael on 10/05/17.
  */
 
 public class ViagemListActivity extends ListActivity {
+
+    private List<Map<String, Object>> viagens;
 
     private AdapterView.OnItemClickListener listener =
             new AdapterView.OnItemClickListener() {
@@ -27,9 +30,12 @@ public class ViagemListActivity extends ListActivity {
                                                 adapterView,
                                         View view, int position,
                                         long id) {
-                    TextView textView = (TextView) view;
-                    String mensagem = "Viagem selecionada: "+
-                            textView.getText();
+
+                    Map<String, Object> mapa =
+                            viagens.get(position);
+
+                    String destino = (String) mapa.get("destino");
+                    String mensagem = "Viagem selecionada "+destino;
                     Toast.makeText(getApplicationContext(),
                             mensagem,
                             Toast.LENGTH_SHORT).show();
@@ -43,19 +49,44 @@ public class ViagemListActivity extends ListActivity {
             @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setListAdapter(
-                new ArrayAdapter<String>(
-                        this,
-                        android.R.layout.simple_list_item_1,
-                        listarViagens()
-                )
-        );
+        viagens = listarViagens();
+
+        String[] de = {"imagem", "destino", "data", "total" };
+        int[] para = {R.id.idtipoviagem, R.id.iddestino,
+            R.id.iddata, R.id.idvalor};
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                viagens,
+                R.layout.layout_lista_viagem,
+                de,
+                para);
+
+        setListAdapter(adapter);
         ListView listView = getListView();
         listView.setOnItemClickListener(listener);
     }
 
-    private List<String> listarViagens(){
-        return Arrays.asList("São José","Palhoça","Biguaçu",
-                "Floripa");
+    private List<Map<String, Object>> listarViagens(){
+
+        List<Map<String, Object>> viagens
+                = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> item =
+                new HashMap<String, Object>();
+        item.put("imagem", R.drawable.negocios);
+        item.put("destino", "São José");
+        item.put("data", "17/05/2017 até o dia 24/05/2017");
+        item.put("total", "Gasto total R$ 300,00");
+        viagens.add(item);
+
+        item = new HashMap<String, Object>();
+        item.put("imagem", R.drawable.lazer);
+        item.put("destino", "Palhoça");
+        item.put("data", "24/05/2017 até o dia 31/05/2017");
+        item.put("total", "Gasto total R$ 150,00");
+        viagens.add(item);
+
+        return viagens;
     }
 }
